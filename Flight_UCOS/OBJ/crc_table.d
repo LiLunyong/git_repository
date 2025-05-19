5b8749ba7603f66bda109f5543c6cff1b7a8a0fc
LiLunyong <3049286548@qq.com>
2025-05-19 17:51:04 +0800

    删除忽略的目录

--- Flight_UCOS/OBJ/crc_table.d
+++
@@ -1,2 +0,0 @@
-..\obj\crc_table.o: ..\HARDWARE\FDILink\CRC_Table.c
-..\obj\crc_table.o: G:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Flight_UCOS/OBJ/nmea0183.d
+++
@@ -1,3 +0,0 @@
-..\obj\nmea0183.o: ..\HARDWARE\GNSS\NMEA0183.c
-..\obj\nmea0183.o: ..\HARDWARE\GNSS\NMEA0183.h
-..\obj\nmea0183.o: G:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
